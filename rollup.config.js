a105130168a0a08d25de2d3e29082b3d703036d1
Michael Martin-Smucker <mlmartin13@gmail.com>
2019-02-17 17:16:33 -0700

    Stub out a reason-react project

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+
+export default {
+  input: 'src/Main.bs.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife'
+  },
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'node_modules/react/index.js': [
+          'Children',
+          'Component',
+          'PureComponent',
+          'PropTypes',
+          'createElement',
+          'Fragment',
+          'cloneElement',
+          'StrictMode',
+          'createFactory',
+          'createRef',
+          'createContext',
+          'isValidElement',
+          'isValidElementType',
+        ],
+        'node_modules/react-dom/index.js': [
+          'render',
+          'hydrate',
+        ],
+      },
+    }),
+  ]
+};
